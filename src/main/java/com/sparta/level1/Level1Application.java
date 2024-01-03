943c5ab939464ee26e9ba0e358264a7a7177d0f8
tls3254 <bikim9923@gmail.com>
2024-01-03 12:55:33 +0900

    init first

---
+++ src/main/java/com/sparta/level1/Level1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.level1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Level1Application.class, args);
+    }
+
+}
